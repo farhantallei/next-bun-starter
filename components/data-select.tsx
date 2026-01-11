99f2af76746922686c2a0d6b35a39f4372fb973c
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2026-01-11 10:59:46 +0700

    update data select

--- components/data-select.tsx
+++ components/data-select.tsx
@@ -1,7 +1,7 @@
 import { Alert02Icon } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { useInfiniteQuery } from "@tanstack/react-query"
-import { createContext, use, useEffect, useId, useState } from "react"
+import { useEffect, useId, useState } from "react"
 import { useInView } from "react-intersection-observer"
 
 import { getPaginatedData } from "@/features/examples/api"
@@ -18,48 +18,13 @@ import {
 } from "./ui/combobox"
 import { Spinner } from "./ui/spinner"
 
-type DataSelectContextValue = {
-  search: string
-  setSearch: (value: string) => void
-}
+export default function DataSelect2() {
+  const queryKey = useId()
 
-const DataSelectContext = createContext<DataSelectContextValue>(
-  {} as DataSelectContextValue,
-)
-
-export default function DataSelect() {
   const [search, setSearch] = useState("")
-
-  return (
-    <DataSelectContext.Provider value={{ search, setSearch }}>
-      <Combobox<{ id: string; title: string }>
-        itemToStringLabel={(item) => item.title}
-        itemToStringValue={(item) => item.id}
-        onOpenChange={(open) => {
-          if (!open) setSearch("")
-        }}
-        onValueChange={() => void setSearch("")}
-        openOnInputClick
-      >
-        <ComboboxInput
-          aria-label="Search items"
-          onChange={(e) => void setSearch(e.target.value)}
-          placeholder="Search items…"
-          showClear
-        />
-        <DataSelectContent />
-      </Combobox>
-    </DataSelectContext.Provider>
-  )
-}
-
-function DataSelectContent() {
-  const { search } = use(DataSelectContext)
 
   const debouncedSearch = useDebounceValue(search, 300)
   const [loadingRef, inView] = useInView()
-
-  const queryKey = useId()
 
   const {
     data,
@@ -96,40 +61,56 @@ function DataSelectContent() {
   }, [inView])
 
   return (
-    <ComboboxPopup>
-      {status === "success" &&
-      (data.pages.length <= 0 || data.pages[0].data.length <= 0) ? (
-        <ComboboxEmpty>No items found.</ComboboxEmpty>
-      ) : null}
-      <ComboboxList>
-        {status === "success" && data.pages.length > 0
-          ? data.pages.map((page) =>
-              page.data.map((data) => (
-                <ComboboxItem key={data.id} value={data}>
-                  {data.title}
-                </ComboboxItem>
-              )),
-            )
-          : null}
-        {hasNextPage ? (
-          <ComboboxStatus
-            render={<div ref={!isFetchingNextPage ? loadingRef : null} />}
-          >
+    <Combobox<{ id: string; title: string }>
+      itemToStringLabel={(item) => item.title}
+      itemToStringValue={(item) => item.id}
+      onOpenChange={(open) => {
+        if (!open) setSearch("")
+      }}
+      onValueChange={() => void setSearch("")}
+      openOnInputClick
+    >
+      <ComboboxInput
+        aria-label="Search items"
+        onChange={(e) => void setSearch(e.target.value)}
+        placeholder="Search items…"
+        showClear
+      />
+      <ComboboxPopup>
+        {status === "success" &&
+        (data.pages.length <= 0 || data.pages[0].data.length <= 0) ? (
+          <ComboboxEmpty>No items found.</ComboboxEmpty>
+        ) : null}
+        <ComboboxList>
+          {status === "success" && data.pages.length > 0
+            ? data.pages.flatMap((page) =>
+                page.data.map((data) => (
+                  <ComboboxItem key={data.id} value={data}>
+                    {data.title}
+                  </ComboboxItem>
+                )),
+              )
+            : null}
+          {hasNextPage ? (
+            <ComboboxStatus
+              render={<div ref={!isFetchingNextPage ? loadingRef : null} />}
+            >
+              <DataSelectSearching />
+            </ComboboxStatus>
+          ) : null}
+        </ComboboxList>
+        {isLoading ? (
+          <ComboboxStatus>
             <DataSelectSearching />
           </ComboboxStatus>
         ) : null}
-      </ComboboxList>
-      {isLoading ? (
-        <ComboboxStatus>
-          <DataSelectSearching />
-        </ComboboxStatus>
-      ) : null}
-      {isError ? (
-        <ComboboxStatus>
-          <DataSelectError />
-        </ComboboxStatus>
-      ) : null}
-    </ComboboxPopup>
+        {isError ? (
+          <ComboboxStatus>
+            <DataSelectError />
+          </ComboboxStatus>
+        ) : null}
+      </ComboboxPopup>
+    </Combobox>
   )
 }
 
